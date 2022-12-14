874cf9d1b36eab8368bd190b80e2c9222625ceed
William Baele <baelewilliam1999@gmail.com>
2022-12-14 16:01:06 +0800

    quick fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_073530) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_073630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_073530) do
     t.datetime "updated_at", null: false
     t.text "content"
     t.date "date"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_073530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
