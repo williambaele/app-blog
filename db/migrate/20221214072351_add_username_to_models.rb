59e43912793bf7e19622d5dc7cac1cc1bb00aa59
William Baele <baelewilliam1999@gmail.com>
2022-12-14 15:57:27 +0800

    users added

---
+++ db/migrate/20221214072351_add_username_to_models.rb
@@ -0,0 +1,5 @@
+class AddUsernameToModels < ActiveRecord::Migration[7.0]
+  def change
+    add_column :models, :username, :string
+  end
+end
---
+++ db/migrate/20221214073433_drop_models.rb
@@ -0,0 +1,5 @@
+class DropModels < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :models
+  end
+end
